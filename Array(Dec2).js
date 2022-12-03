094702a23fbbc848021ec8622f742329951dbc85
ASPLAP3270 <dhanushbaskaran2000@gmail.com>
2022-12-03 16:07:45 +0530

    javascriptPractices(Dec2)

---
+++ Array(Dec2).js
@@ -0,0 +1,20 @@
+const shoppingList = ['bread', 'milk', 'cheese', 'hummus', 'noodles',[2,3,4]]
+console.log(shoppingList)
+console.log(shoppingList[0])
+shoppingList[0] = "bun"
+console.log(shoppingList)
+console.log(shoppingList[5][1])
+
+const birds = ['parrot','falcon','owl']
+console.log(birds.indexOf('owl'))
+birds.push('eagle') 
+console.log(birds)
+console.log(birds.pop())
+birds.unshift('cockatail')
+console.log(birds.shift())
+console.log(birds.splice(0,2))
+
+// for of 
+
+var 
+
